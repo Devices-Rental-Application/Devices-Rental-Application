5ba67e9ea095d7fb5265e8b6c464ed1d09cc24a7
youngjaeee <youngjaeee@chungbuk.ac.kr>
2022-05-29 14:18:12 +0900

    added mypage

--- app/src/main/java/com/neonadeuri/devices_rental_application/MainActivity.java
+++ app/src/main/java/com/neonadeuri/devices_rental_application/MainActivity.java
@@ -2,13 +2,33 @@ package com.neonadeuri.devices_rental_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnMyPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMyPage = (Button) findViewById(R.id.mypagebutton);
+
+        btnMyPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMyPage();
+            }
+        });
+
+
+        }
+    public void openMyPage() {
+        Intent intent = new Intent(this, myPageActivity.class);
+        startActivity(intent);
     }
 }
