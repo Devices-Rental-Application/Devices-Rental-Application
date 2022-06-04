95089c08da77f28c20388865dfc8296fc0a6c3d3
jong_jh <rhfksl0115@gmail.com>
2022-06-04 14:53:13 +0900

    interestRV fragment 연동

--- app/src/main/java/com/neonadeuri/devices_rental_application/FragmentDibs.java
+++ app/src/main/java/com/neonadeuri/devices_rental_application/FragmentDibs.java
@@ -9,8 +9,15 @@ package com.neonadeuri.devices_rental_application;
         import androidx.annotation.NonNull;
         import androidx.annotation.Nullable;
         import androidx.fragment.app.Fragment;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
 
 public class FragmentDibs extends Fragment {
+
+    static RecyclerView recyclerView3;
+    static mainAdapter adapter3;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dibs_page, container, false);
 
@@ -19,6 +26,22 @@ public class FragmentDibs extends Fragment {
                 return true;
             }
         });
+
+
+        recyclerView3 = view.findViewById(R.id.interestRV);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        recyclerView3.setLayoutManager(linearLayoutManager);
+        adapter3 = new mainAdapter();
+
+        for(int i=1;i<=10;i++){
+            if(i%2==0)
+                adapter3.addItem(new Device(R.drawable.orcam,i+"번째 사람",i));
+            else
+                adapter3.addItem(new Device(R.drawable.tackplus,i+"번째 사람",i));
+
+        }
+        recyclerView3.setAdapter(adapter3);
 
         return view;
     }
