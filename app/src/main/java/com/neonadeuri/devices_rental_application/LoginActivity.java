c348e03e2a19caf540e6c0082e5e240565fae82e
jong_jh <rhfksl0115@gmail.com>
2022-06-08 14:27:04 +0900

    login 화면 제작

---
+++ app/src/main/java/com/neonadeuri/devices_rental_application/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.neonadeuri.devices_rental_application;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
